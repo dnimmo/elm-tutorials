5b181a97bbb6eb19511299d045473ef063246917
David Nimmo <dnimmo@gmail.com>
2016-10-10 21:06:37 -0400

    Exercise2 in "02" completed

---
+++ knowthen/02 more on functions/Exercise2.elm
@@ -0,0 +1,13 @@
+module Main exposing (..)
+
+import Html
+import String
+
+(~=) : String -> String -> Bool
+(~=) a b =
+  String.left 1 a == String.left 1 b
+
+main =
+  (~=) "Try" "Test"
+  |> toString
+  |> Html.text
