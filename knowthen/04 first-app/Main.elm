355940f6c2dc2d4e4016666db19eb7108fb5c0d9
David Nimmo <dnimmo@gmail.com>
2016-10-15 22:13:36 +0100

    completed "first app"

--- knowthen/04 first-app/Main.elm
+++ knowthen/04 first-app/Main.elm
@@ -1,68 +1,91 @@
 module Main exposing (..)
 
+import String
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Html.App as App
 
-
--- model
-
-
 type alias Model =
-    Int
-
+  { calories : Int
+  , input : Int
+  , error : Maybe String
+  }
 
 initModel : Model
 initModel =
-    0
-
-
-
--- update
-
+  { calories = 0
+  , input = 0
+  , error = Nothing
+  }
 
 type Msg
-    = AddCalorie
-    | Clear
+  = Input String
+  | AddCalories
+  | Clear
 
 
 update : Msg -> Model -> Model
 update msg model =
-    case msg of
-        AddCalorie ->
-            model + 1
+  case msg of
+    Input val ->
+      case String.toInt val of
+        Ok input ->
+          { model
+              | input = input
+              , error = Nothing
+          }
 
-        Clear ->
-            initModel
+        Err err ->
+          { model
+              | input = 0
+              , error = Just err
+          }
 
+    AddCalories ->
+      { model
+        | calories = model.calories + model.input
+        , input = 0
+      }
 
-
--- view
+    Clear ->
+      initModel
 
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ h3 []
-            [ text ("Total Calories: " ++ (toString model)) ]
-        , button
-            [ type' "button"
-            , onClick AddCalorie
-            ]
-            [ text "Add" ]
-        , button
-            [ type' "button"
-            , onClick Clear
-            ]
-            [ text "Clear" ]
+  div []
+    [ h3 []
+      [ text ("Calorie Counter: " ++ (toString model.calories)) ]
+    , input
+        [ type' "text"
+        , placeholder "Enter calories"
+        , onInput Input
+        , value
+            (if model.input == 0 then
+              ""
+             else
+               toString model.input
+            )
         ]
-
+        []
+    , div [] [ text (Maybe.withDefault "" model.error) ]
+    , button
+        [ type' "button"
+        ,  onClick AddCalories
+        ]
+        [ text "Add" ]
+    , button
+        [ type' "button"
+        , onClick Clear
+        ]
+        [ text "Clear"]
+    ]
 
 main : Program Never
 main =
-    App.beginnerProgram
-        { model = initModel
-        , update = update
-        , view = view
-        }
+  App.beginnerProgram
+      { model = initModel
+      , update = update
+      , view = view
+      }
