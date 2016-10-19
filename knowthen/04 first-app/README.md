232af499da47d6b43756fb76fb5fcb049b7258ec
David Nimmo <dnimmo@gmail.com>
2016-10-19 11:37:53 +0100

    tidied up extra files after completion

--- knowthen/04 first-app/README.md
+++ knowthen/04 first-app/README.md
@@ -2,5 +2,3 @@
 
 The `Main.elm` file contains the initial version of our calorie counter.  
 Use it as a starting point for adding an input field to key calorie values into the counter.
-
-If you get stuck, you can find the code for the improved calorie counter in the folder `first-app-improved`
--- knowthen/05 scorekeeper-starter/Main.elm
+++ knowthen/05 scorekeeper-starter/Main.elm
@@ -1,6 +1,5 @@
 module Main exposing (..)
 
-import String
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
@@ -15,7 +14,6 @@ type alias Model =
     , name : String
     , playerId : Maybe Int
     , plays : List Play
-    , nextId : Int
     }
 
 
@@ -40,7 +38,6 @@ initModel =
     , name = ""
     , playerId = Nothing
     , plays = []
-    , nextId = 0
     }
 
 
@@ -56,142 +53,11 @@ type Msg
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        Edit player ->
-            { model
-                | name = player.name
-                , playerId = Just player.id
-            }
-
-        Score player points ->
-            score model player points
-
         Input name ->
             { model | name = name }
 
-        Save ->
-            if (String.isEmpty model.name) then
-                model
-            else
-                save model
-
-        Cancel ->
-            { model
-                | name = initModel.name
-                , playerId = Nothing
-            }
-
-        DeletePlay play ->
-            deletePlay model play
-
-
-score : Model -> Player -> Int -> Model
-score model scorer points =
-    let
-        newPlayers =
-            List.map
-                (\player ->
-                    if player.id == scorer.id then
-                        { player
-                            | points = player.points + points
-                        }
-                    else
-                        player
-                )
-                model.players
-
-        play =
-            Play (List.length model.plays) scorer.id scorer.name points
-    in
-        { model
-            | players = newPlayers
-            , plays = play :: model.plays
-        }
-
-
-save : Model -> Model
-save model =
-    case model.playerId of
-        Just id ->
-            edit model id
-
-        Nothing ->
-            add model
-
-
-add : Model -> Model
-add model =
-    let
-        player =
-            Player model.nextId model.name 0
-
-        newPlayers =
-            player :: model.players
-    in
-        { model
-            | players = newPlayers
-            , name = ""
-            , nextId = model.nextId + 1
-        }
-
-
-edit : Model -> Int -> Model
-edit model id =
-    let
-        newPlayers =
-            List.map
-                (\player ->
-                    if player.id == id then
-                        { player | name = model.name }
-                    else
-                        player
-                )
-                model.players
-
-        newPlays =
-            List.map
-                (\play ->
-                    if play.playerId == id then
-                        { play | name = model.name }
-                    else
-                        play
-                )
-                model.plays
-    in
-        { model
-            | players = newPlayers
-            , plays = newPlays
-            , name = ""
-            , playerId = Nothing
-        }
-
-
-deletePlay : Model -> Play -> Model
-deletePlay model play =
-    let
-        newPlays =
-            List.filter (\p -> p.id /= play.id) model.plays
-
-        newPlayers =
-            List.map
-                (\player ->
-                    if player.id == play.playerId then
-                        { player | points = player.points - 1 * play.points }
-                    else
-                        player
-                )
-                model.players
-    in
-        { model | plays = newPlays, players = newPlayers }
-
-
-editModeClasses : Model -> String -> String
-editModeClasses model baseClasses =
-    case model.playerId of
-        Nothing ->
-            baseClasses
-
-        Just id ->
-            baseClasses ++ " edit"
+        _ ->
+            model
 
 
 
@@ -202,9 +68,7 @@ view : Model -> Html Msg
 view model =
     div [ class "scoreboard" ]
         [ h1 [] [ text "Score Keeper" ]
-        , playerSection model
         , playerForm model
-        , playSection model
         ]
 
 
@@ -212,8 +76,7 @@ playerForm : Model -> Html Msg
 playerForm model =
     Html.form [ onSubmit Save ]
         [ input
-            [ class (editModeClasses model "")
-            , type' "text"
+            [ type' "text"
             , placeholder "Add/Edit Player..."
             , onInput Input
             , value model.name
@@ -222,112 +85,6 @@ playerForm model =
         , button [ type' "submit" ] [ text "Save" ]
         , button [ type' "button", onClick Cancel ] [ text "Cancel" ]
         ]
-
-
-playSection : Model -> Html Msg
-playSection model =
-    div []
-        [ playListHeader
-        , playList model
-        ]
-
-
-playListHeader : Html Msg
-playListHeader =
-    header []
-        [ div [] [ text "Plays" ]
-        , div [] [ text "Points" ]
-        ]
-
-
-playList : Model -> Html Msg
-playList model =
-    model.plays
-        |> List.map play
-        |> ul []
-
-
-play : Play -> Html Msg
-play play =
-    li []
-        [ i
-            [ class "remove"
-            , onClick (DeletePlay play)
-            ]
-            []
-        , div [] [ text play.name ]
-        , div [] [ text (toString play.points) ]
-        ]
-
-
-playerSection : Model -> Html Msg
-playerSection model =
-    div []
-        [ playerListHeader
-        , playerList model
-        , pointTotal model
-        ]
-
-
-playerListHeader : Html Msg
-playerListHeader =
-    header []
-        [ div [] [ text "Name" ]
-        , div [] [ text "Points " ]
-        ]
-
-
-playerList : Model -> Html Msg
-playerList model =
-    model.players
-        |> List.sortBy .name
-        |> List.map (player (model.playerId /= Nothing))
-        |> ul []
-
-
-player : Bool -> Player -> Html Msg
-player isEditMode player =
-    li []
-        [ i
-            [ class "edit"
-            , onClick (Edit player)
-            ]
-            []
-        , div
-            [ class
-                (if isEditMode then
-                    "edit"
-                 else
-                    ""
-                )
-            ]
-            [ text player.name ]
-        , button
-            [ type' "button"
-            , onClick (Score player 2)
-            ]
-            [ text "2pts" ]
-        , button
-            [ type' "button"
-            , onClick (Score player 3)
-            ]
-            [ text "3pts" ]
-        , div []
-            [ text (toString player.points) ]
-        ]
-
-
-pointTotal : Model -> Html Msg
-pointTotal model =
-    let
-        total =
-            List.map .points model.plays
-                |> List.sum
-    in
-        footer []
-            [ div [] [ text "Total: " ]
-            , div [] [ text (toString total) ]
-            ]
 
 
 main : Program Never
